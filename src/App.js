05e4fbeb0278de41d21f43adef298ea64941e78c
Sowmiya <113595981+sowmiyapalani63@users.noreply.github.com>
2024-12-26 13:38:48 +0530

    "Header and footer added"

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@
 import PreLoader from './Components/Common/PreLoader';
 import Agents from './Pages/Agents/Agents';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Common/Header';
+import FooterHead from './Components/Common/FooterHead';
+import Footer from './Components/Common/Footer';
 import BlogList from './Pages/Blog/BlogList';
+import Contact from './Pages/Contact/Contact';
+
+
 function App() {
   return (
     <>
  <Router>
+ <Header />
       <PreLoader />
       <Routes>
         <Route path="/" element={<Agents />} />
@@ -14,7 +21,10 @@ function App() {
         {/* Blog Routes */}
 
         <Route path="/Blog-list" element={<BlogList />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
+      <FooterHead />
+      <Footer />
     </Router>
     </>
   );
---
+++ src/Components/Common/Footer.jsx
@@ -0,0 +1,177 @@
+import React from 'react'
+import  logo2 from '../../Assests/images/logo/logo-2@2x.png'
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+            <div class="tf-container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="footer-top">
+                            <div class="footer-logo">
+                                <a href="index.html">
+                                    <img id="logo_footer" src={logo2} alt="logo-footer" />
+                                </a>
+                            </div>
+                            <div class="footer-contact">
+                                <div class="contact-item">
+                                    <div class="icons">
+                                        <svg width="48" height="49" viewBox="0 0 48 49" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M43.9989 34.34V40.34C44.0012 40.897 43.8871 41.4483 43.6639 41.9586C43.4408 42.469 43.1135 42.9271 42.703 43.3037C42.2926 43.6802 41.808 43.9669 41.2804 44.1454C40.7527 44.3238 40.1936 44.3901 39.6389 44.34C33.4846 43.6712 27.5729 41.5682 22.3789 38.2C17.5465 35.1293 13.4496 31.0323 10.3789 26.2C6.99885 20.9824 4.89538 15.0419 4.23889 8.85995C4.18891 8.30688 4.25464 7.74947 4.43189 7.2232C4.60914 6.69693 4.89403 6.21333 5.26842 5.80319C5.64281 5.39306 6.0985 5.06537 6.60647 4.84099C7.11444 4.61662 7.66357 4.50047 8.21889 4.49995H14.2189C15.1895 4.4904 16.1305 4.83411 16.8664 5.46702C17.6024 6.09992 18.083 6.97884 18.2189 7.93995C18.4721 9.86008 18.9418 11.7454 19.6189 13.5599C19.888 14.2758 19.9462 15.0538 19.7867 15.8017C19.6272 16.5496 19.2566 17.2362 18.7189 17.78L16.1789 20.3199C19.026 25.327 23.1718 29.4728 28.1789 32.32L30.7189 29.78C31.2627 29.2422 31.9492 28.8716 32.6971 28.7121C33.4451 28.5526 34.223 28.6109 34.9389 28.8799C36.7534 29.5571 38.6388 30.0267 40.5589 30.28C41.5304 30.417 42.4177 30.9064 43.052 31.6549C43.6862 32.4035 44.0232 33.3591 43.9989 34.34Z"
+                                                stroke="#F1913D" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                        </svg>
+                                    </div>
+                                    <div class="content">
+                                        <div class="title text-1">
+                                            Call us
+                                        </div>
+                                        <h6>
+                                            (603) 555-0123
+                                        </h6>
+                                    </div>
+                                </div>
+                                <div class="contact-item">
+                                    <div class="icons">
+                                        <svg width="48" height="49" viewBox="0 0 48 49" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M40 8.5H8C5.79086 8.5 4 10.2909 4 12.5V36.5C4 38.7091 5.79086 40.5 8 40.5H40C42.2091 40.5 44 38.7091 44 36.5V12.5C44 10.2909 42.2091 8.5 40 8.5Z"
+                                                stroke="#F1913D" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                            <path
+                                                d="M44 14.5L26.06 25.9C25.4425 26.2869 24.7286 26.492 24 26.492C23.2714 26.492 22.5575 26.2869 21.94 25.9L4 14.5"
+                                                stroke="#F1913D" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                        </svg>
+
+                                    </div>
+                                    <div class="content">
+                                        <div class="title text-1">
+                                            Nee live help
+                                        </div>
+                                        <h6 class="fw-4">
+                                            proty-support@gmail.com
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-main">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6">
+                                <div class="footer-menu-list footer-col-block style-2">
+                                    <h5 class="title lh-30 title-desktop">About us</h5>
+                                    <h5 class="title lh-30 title-mobile">About us</h5>
+                                    <ul class="tf-collapse-content">
+                                        <li><a href="contact.html">Contact</a></li>
+                                        <li><a href="service-details.html">Why choose us?</a></li>
+                                        <li><a href="#">Customer reviews</a></li>
+                                        <li><a href="agents.html">Our team</a></li>
+                                        <li><a href="career.html">Careers with realty</a></li>
+                                        <li><a href="career.html">Work with us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="footer-menu-list footer-col-block">
+                                    <h5 class="title lh-30 title-desktop">Popular house</h5>
+                                    <h5 class="title lh-30 title-mobile">Popular house</h5>
+                                    <ul class="tf-collapse-content">
+                                        <li><a href="property-gird.html">#Penthouses</a></li>
+                                        <li><a href="property-gird.html">#Villa</a></li>
+                                        <li><a href="property-gird.html">#Smart home</a></li>
+                                        <li><a href="property-gird.html">#Apartments</a></li>
+                                        <li><a href="property-gird.html">#Office</a></li>
+                                        <li><a href="property-gird.html">#Bungalow</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="footer-menu-list footer-col-block style-2">
+                                    <h5 class="title lh-30 title-desktop">Quick links</h5>
+                                    <h5 class="title lh-30 title-mobile">Quick links</h5>
+                                    <ul class="tf-collapse-content">
+                                        <li><a href="#">Terms of use</a></li>
+                                        <li><a href="#">Privacy policy</a></li>
+                                        <li><a href="#">Our services</a></li>
+                                        <li><a href="contact.html">Contact support</a></li>
+                                        <li><a href="#">Pricing plans</a></li>
+                                        <li><a href="faq.html">FAQs</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="footer-menu-list newsletter ">
+                                    <h5 class="title lh-30 ">Newsletter</h5>
+                                    <form class="form-newsletter" id="subscribe-form" action="#" method="post"
+                                        accept-charset="utf-8" data-mailchimp="true">
+                                        <p class="text-1">Sign up to receive the latest articles</p>
+                                        <div id="subscribe-content">
+                                            <fieldset>
+                                                <input class="" type="email" placeholder="Your email address"
+                                                    id="subscribe-email" name="text" tabindex="2" aria-required="true"
+                                                    required="" />
+                                            </fieldset>
+                                            <div class="button-submit">
+                                                <button class="tf-btn bg-color-primary w-full" type="button"
+                                                    id="subscribe-button">Subscribe</button>
+                                            </div>
+                                            <div class="checkbox-item">
+                                                <label>
+                                                    <span class="text-2">I have read and agree to the terms &
+                                                        conditions</span>
+                                                    <input type="checkbox" />
+                                                    <span class="btn-checkbox"></span>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div id="subscribe-msg"></div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="footer-bottom">
+                            <p>Copyright © 2024 <span class="fw-7">PROTY - REAL ESTATE</span> . Designed & Developed
+                                by
+                                <a href="#">Themesflat</a>
+                            </p>
+                            <div class="wrap-social">
+                                <div class="text-3  fw-6 text-white">Follow us</div>
+                                <ul class="tf-social ">
+                                    <li>
+                                        <a href="#">
+                                            <i class="icon-fb"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="icon-X"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="icon-linked"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="icon-ins"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+  )
+}
+
+export default Footer
